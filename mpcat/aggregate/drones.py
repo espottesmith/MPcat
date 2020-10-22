0f88dba934a98f4c89c129aeefb1b9fbc58d202a
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2020-10-22 09:47:50 -0400

    Hopefully fixed AutoTSBuilderDrone; previously used syntax appropriate for maggma, but maggma will no longer be used in MPcat

--- mpcat/aggregate/drones.py
+++ mpcat/aggregate/drones.py
@@ -310,9 +310,9 @@ class AutoTSBuilderDrone:
         paths = [e for e in records.keys()]
         paths_names = [e.as_posix() for e in paths]
 
-        cursor = self.db.database[self.db.collection].query(
+        cursor = self.db.database[self.db.data_collection].find(
             {"path": {"$in": paths_names}},
-            ["path", "hash", "last_updated"]
+            {"_id": 0, "path": 1, "hash": 1, "last_updated": 1}
         )
 
         db_record_log = {doc["path"]: doc["hash"] for doc in cursor}
@@ -341,7 +341,7 @@ class AutoTSBuilderDrone:
             docs.append(doc)
 
         if len(docs) > 0:
-            self.db.update(docs, key="path")
+            self.db.update_data_doc(docs, key="path")
 
     def build(self):
         """
@@ -359,198 +359,3 @@ class AutoTSBuilderDrone:
         to_update = self.find_records_to_update(mapping)
 
         self.update_targets(to_update)
-
-
-# class AutoTSDroneMaggma(Drone):
-#     """
-#     A drone to parse AutoTS calculations and enter them into a database.
-#     """
-#
-#     def __init__(self, store, path: Path):
-#         super().__init__(store=store, path=path)
-#
-#     def compute_record_identifier_key(self, doc: Document) -> str:
-#         """
-#         Compute the RecordIdentifier key that this document correspond to
-#
-#         Args:
-#             doc: document which the record identifier key will be inferred from
-#
-#         Returns:
-#             RecordIdentifiierKey
-#         """
-#
-#         if "AutoTS." in doc.path.parts[-1]:
-#             return doc.path.parent.as_posix()
-#         else:
-#             return doc.path.as_posix()
-#
-#     def compute_record_identifier(self, record_key: str, doc_list: List[Document]) -> RecordIdentifier:
-#         """
-#         Compute meta data for this list of documents, and generate a RecordIdentifier object
-#         :param record_key: record keys that indicate a record
-#         :param doc_list: document on disk that this record include
-#         :return:
-#             RecordIdentifier that represent this doc_list
-#         """
-#         recordIdentifier = RecordIdentifier(
-#             last_updated=datetime.now(), documents=doc_list, record_key=record_key
-#         )
-#         recordIdentifier.state_hash = recordIdentifier.compute_state_hash()
-#         return recordIdentifier
-#
-#     @staticmethod
-#     def get_documents_calc_dir(calc_dir: Path) -> List[Document]:
-#         """
-#         Identify all important documents within an AutoTS calculation directory.
-#
-#         Args:
-#             calc_dir: Path object that indicates a path to an individual AutoTS
-#                 calculation
-#
-#         Returns:
-#             List of Documents
-#         """
-#
-#         root_contents = [f for f in os.listdir(calc_dir.as_posix())]
-#
-#         subdirs = [calc_dir / f for f in root_contents if (calc_dir / f).is_dir() and "AutoTS" in f]
-#         files = [f for f in root_contents if (calc_dir / f).is_file()]
-#
-#         files_paths = list()
-#         for file in files:
-#             if "AutoTS" in file and file.split(".", maxsplit=2)[-1] in ["mae", "out", "in",
-#                                                                         "mae.gz", "out.gz", "in.gz"]:
-#                 files_paths.append(calc_dir / file)
-#             elif ("pro" in file or "rct" in file) and file.split(".", maxsplit=2)[-1] in ["mae",
-#                                                                                           "mae.gz"]:
-#                 files_paths.append(calc_dir / file)
-#             elif file.endswith(".in") or file.endswith(".in.gz"):
-#                 files_paths.append(calc_dir / file)
-#
-#         for subdir in subdirs:
-#             sub_files = [f for f in os.listdir(subdir.as_posix())]
-#             sub_paths = [subdir / f for f in sub_files]
-#             for ff, file in sub_files:
-#                 if file.split(".", maxsplit=2)[-1] in ["mae", "out", "in",
-#                                                        "mae.gz", "out.gz", "in.gz"]:
-#                     files_paths.append(sub_paths[ff])
-#
-#         return [Document(path=fp, name=fp.name) for fp in files_paths]
-#
-#     def read(self, path: Path) -> List[RecordIdentifier]:
-#         """
-#         Given a folder path to a data folder, read all the files, and return a dictionary
-#         that maps each RecordKey -> [RecordIdentifier]
-#
-#         ** Note: require user to implement the function computeRecordIdentifierKey
-#
-#         Args:
-#             path: Path object that indicate a path to a data folder
-#
-#         Returns:
-#             List of Record Identifiers
-#         """
-#
-#         calc_dirs = [path / f for f in os.listdir(path.as_posix()) if (path / f).is_dir()]
-#
-#         record_identifiers = list()
-#         for calc_dir in calc_dirs:
-#             documents = self.get_documents_calc_dir(calc_dir)
-#             document_names = [doc.name for doc in documents]
-#
-#             if len(documents) == 0:
-#                 continue
-#             elif "autots.in" not in document_names or "AutoTS.T9XnCsLi.out" not in document_names:
-#                 continue
-#             else:
-#                 record_identifiers.append(self.compute_record_identifier(calc_dir.as_posix(),
-#                                                                          documents))
-#
-#         return record_identifiers
-#
-#     def compute_data(self, recordID: RecordIdentifier) -> Dict:
-#         """
-#         User can specify what raw data they want to save from the Documents that this recordID refers to
-#
-#         Args:
-#             recordID: recordID that needs to be re-saved
-#
-#         Returns:
-#             Dictionary of NAME_OF_DATA -> DATA
-#                         ex:
-#                             for a recordID refering to 1,
-#                             {
-#                                 "citation": cite.bibtex ,
-#                                 "text": data.txt
-#                             }
-#         """
-#         d = dict()
-#         d["schema"] = {"code": "mpcat", "version": version}
-#         d["path"] = recordID.record_key
-#
-#         documents = recordID.documents
-#         document_names = [doc.name for doc in documents]
-#
-#         # If no input or no output - should never happen
-#         if "autots.in" not in document_names or "AutoTS.T9XnCsLi.out" not in document_names:
-#             raise ValueError("Input or output file missing in recordID!")
-#
-#         autots_input = AutoTSInput.from_file(os.path.join(d["path"], "autots.in"),
-#                                              read_molecules=True)
-#
-#         d["input"] = {"reactants": autots_input.reactants,
-#                       "products": autots_input.products,
-#                       "autots_variables": autots_input.autots_variables,
-#                       "gen_variables": autots_input.gen_variables}
-#
-#         autots_output = AutoTSOutput(os.path.join(d["path"],
-#                                                   "AutoTS.T9XnCsLi.out"))
-#
-#         d["calculation_names"] = autots_output.data["calculations"]
-#
-#         d["warnings"] = autots_output.data["warnings"]
-#         d["errors"] = autots_output.data["errors"]
-#         d["completed"] = autots_output.data["complete"]
-#         d["walltime"] = autots_output.data["walltime"]
-#         if d["completed"]:
-#             d["output"] = {"temperature": autots_output.data["output"]["temperature_dependence"],
-#                            "free_energy": autots_output.data["output"]["gibbs_energies"],
-#                            "separated_energies": autots_output.data["output"]["separated_energies"],
-#                            "complex_energies": autots_output.data["output"]["complex_energies"],
-#                            "optimized_structure_energies": autots_output.data["output"]["struct_energies"]}
-#             d["diagnostic"] = autots_output.data["diagnostic"]
-#         else:
-#             d["output"] = dict()
-#             #TODO: could actually parse diagnostic for failed calcs; just requires tweaking of regex
-#             d["diagnostic"] = dict()
-#
-#         d["calcs"] = list()
-#         for calculation in d["calculation_names"]:
-#             found = False
-#             for document in documents:
-#                 if document.name in [calculation + ".out",
-#                                      calculation + ".out.gz"]:
-#                     found = True
-#                     jag_out = JagOutput(document.path.to_posix(),
-#                                         allow_failure=True,
-#                                         parse_molecules=True)
-#                     d["calcs"].append(jag_out.data)
-#
-#             if not found:
-#                 print("Expected calculation {} missing from path!".format(calculation))
-#
-#         full_paths = [d for d in documents if "full_path.mae" in d.name]
-#         if len(full_paths) > 0:
-#             d["output"]["path_molecules"] = maestro_file_to_molecule(full_paths[0].path.to_posix())
-#
-#         rct_complexes = [d for d in documents if "reactant_complex.mae" in d.name]
-#         if len(rct_complexes) > 0:
-#             d["output"]["reactant_complex"] = maestro_file_to_molecule(rct_complexes[0].path.to_posix())[0]
-#
-#         pro_complexes = [d for d in documents if "product_complex.mae" in d.name]
-#         if len(pro_complexes) > 0:
-#             d["output"]["product_complex"] = maestro_file_to_molecule(pro_complexes[0].path.to_posix())[0]
-#
-#         d["last_updated"] = datetime.utcnow()
-#         return d
