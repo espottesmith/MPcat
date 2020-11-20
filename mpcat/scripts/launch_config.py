fd3aaaf13bef300e5aa864291e996f794fa5bb08
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2020-11-20 15:27:25 -0800

    Added a way to launch from a config file

---
+++ mpcat/scripts/launch_config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+
+from monty.serialization import loadfn
+
+from mpcat.aggregate.database import CatDB
+from mpcat.automate.generate_calcs import launch_jobs_from_queue
+
+
+def main():
+    mpcat_config = os.getenv("MPCAT_CONFIG")
+    if mpcat_config:
+        config_file = os.path.join(mpcat_config, "config.json")
+    else:
+        config_file = "config.json"
+
+    config = loadfn(config_file)
+
+    db = CatDB.from_db_file(config["db_file"])
+
+    if str(config["num_launches"]).lower() == "infinite":
+        while True:
+            try:
+                launch_jobs_from_queue(db, config["base_dir"],
+                                       num_launches=1,
+                                       by_priority=config.get("use_priority", False),
+                                       query=config.get("query"),
+                                       num_cores=int(config.get("num_cores")),
+                                       host=config.get("host"),
+                                       command_line_args={"nsubjobs": 1, "WAIT": None})
+            except ValueError:
+                break
+    else:
+        launch_jobs_from_queue(db, config["base_dir"],
+                               num_launches=int(config["num_launches"]),
+                               by_priority=config.get("use_priority", False),
+                               query=config.get("query"),
+                               num_cores=int(config.get("num_cores")),
+                               host=config.get("host"),
+                               command_line_args={"nsubjobs": 1, "WAIT": None})
+
+
+if __name__ == "__main__":
+    main()
