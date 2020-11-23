70d8a89942f970bce4107149b893676988e7c73d
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2020-11-23 15:03:04 -0800

    Small change to recent script

--- mpcat/scripts/reset_unlaunched.py
+++ mpcat/scripts/reset_unlaunched.py
@@ -29,13 +29,13 @@ def main():
     for calc_dir in [x for x in base_dir.iterdir() if x.is_dir()]:
         files = [e.name for e in calc_dir.iterdir() if e.is_file()]
 
-        rxn_id = None
         if "autots.in" in files and "AutoTS.T9XnCsLi.out" not in files:
+            rxn_id = None
             if calc_dir.name.startswith("launcher"):
                 rxn_id = int(calc_dir.name.split("_")[1])
                 to_restart.append(rxn_id)
-        if rxn_id is None:
-            print("Could not determine rxnid for", calc_dir.name)
+            if rxn_id is None:
+                print("Could not determine rxnid for", calc_dir.name)
 
     result = db.database[db.queue_collection].update_many({"rxnid": {"$in": to_restart}},
                                                           {"$set": {"state": "READY"}})
